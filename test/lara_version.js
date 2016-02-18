e7e57eb3a16aada29d00102b7528caf234dff0d1
Lance <lancelotmao@outlook.com>
2016-02-18 17:36:22 +0800

    minor

--- test/lara_version.js
+++ test/lara_version.js
@@ -4,20 +4,14 @@
 var http = require('http');
 var fs = require('fs');
 
-var options = {
-    host: '127.0.0.1',
-    port: 6666,
-    path: '/version?appid=blue&bundleName=test',
-    method: 'POST'
-};
-
-var req = http.request(options, function(res) {
-  	console.log('STATUS: ' + res.statusCode);
- 	  console.log('HEADERS: ' + JSON.stringify(res.headers));
-  	res.setEncoding('utf8');
-  	res.on('data', function (chunk) {
-    	console.log('BODY: ' + chunk);
-  	});
+var request = http.get("http://127.0.0.1:6666/version?appid=blue&bundleName=test", function(res) {
+	if (res.statusCode == 404) {
+		res.setEncoding('utf8');
+	  	res.on('data', function (chunk) {
+	    	console.log('BODY: ' + chunk);
+	  	});
+	} else {
+		var file = fs.createWriteStream("manifest.xml");
+		res.pipe(file);
+	}
 });
-
-req.end();
