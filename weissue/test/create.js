10a3dfa9ec084c7e31dec6dea6b8f6d091f24a79
Lance <lancelotmao@outlook.com>
2016-02-27 22:29:19 +0800

    implement list

--- weissue/test/create.js
+++ weissue/test/create.js
@@ -1,6 +1,6 @@
 
 var data = {title: 'I found an issue', description: 'this is a naughty bug', submittedBy: 'lance', appid: 'jupiter', versionCode:'1'}
-
+var listParam = {appid: 'jupiter'};
 /*
  * test for http
  */
@@ -12,20 +12,43 @@ var HOST = '127.0.0.1';
 // var HOST = '120.25.219.17';
 
 var options = {
-    host: HOST,
-    port: 7000,
-    path: '/weissue/api/create',
-    method: 'POST'
+      host: HOST,
+      port: 7000,
+      path: '/weissue/api/create',
+      method: 'POST'
 };
 
 var req = http.request(options, function(res) {
       console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
+      console.log('HEADERS: ' + JSON.stringify(res.headers));
       res.setEncoding('utf8');
       res.on('data', function (chunk) {
-      console.log('BODY: ' + chunk);
+            console.log('BODY: ' + chunk);
+      });
+
+      res.on('end', function(res){
+            listIssues();
       });
 });
 
 req.end(JSON.stringify(data));
+
+function listIssues() {
+      console.log('listing issues...');
+      var options = {
+            host: HOST,
+            port: 7000,
+            path: '/weissue/api/list',
+            method: 'POST'
+      };
+      var req = http.request(options, function(res) {
+            console.log('STATUS: ' + res.statusCode);
+            console.log('HEADERS: ' + JSON.stringify(res.headers));
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                  console.log('BODY: ' + chunk);
+            });
+      });
+      req.end(JSON.stringify(listParam));
+}
 
--- weissue/weissue.js
+++ weissue/weissue.js
@@ -1,11 +1,11 @@
 /*
  * http server
-*/
-var http = require('http');
-var url = require("url");
-var mysql = require('mysql');
+ */
+ var http = require('http');
+ var url = require("url");
+ var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+ var connection = mysql.createConnection({
   host: '120.25.219.17',
   port: 3306,
   user: 'lara',
@@ -13,21 +13,13 @@ var connection = mysql.createConnection({
   database: 'weissue'
 });
 
-connection.connect();
+ connection.connect();
 
-connection.query('select * from issue;', function(err, rows, fields){
-  if (err) {
-    console.log(err);
-    connection.end();
-  } else {
-    console.log('', rows);  
-  }
-});
-
-http.createServer(function (req, res) {
+ http.createServer(function (req, res) {
   var parsedUrl = url.parse(req.url, true);
   var pathname = parsedUrl.pathname;
 
+  console.log('responding to: ' + pathname);
   var postData = "";
 
   req.on("data", function (postDataChunk) {
@@ -37,20 +29,18 @@ http.createServer(function (req, res) {
   req.on("end", function () {
     var status = 200;
     var msg;
-    var finished = true;
 
     console.log('postData: ' + postData);
 
     var postObj = JSON.parse(postData);
+    var appid = postObj['appid'];
     
     switch (parsedUrl.pathname) {
       case '/weissue/api/create':
       {
-        finished = false;
         var title = postObj['title'];
         var description = postObj['description'];
         var submittedBy = postObj['submittedBy'];
-        var appid = postObj['appid'];
         var versionCode = postObj['versionCode'];
         if (title == null || description == null || submittedBy == null || appid == null || versionCode == null) {
           msg = 'title, description, submittedBy, appid, versionCode cannot be null';
@@ -63,8 +53,7 @@ http.createServer(function (req, res) {
             if (err) {
               console.log(err);
               connection.end();
-            } else {
-              console.log('', rows);  
+            } else { 
               msg = 'create issue success';
               sendResponse(res, status, msg);
             }
@@ -73,24 +62,31 @@ http.createServer(function (req, res) {
       }
       break;
 
-      default:
-      status = 404;
-      msg = 'path not found ' + pathname;
+      case '/weissue/api/list':
+      {
+        var sql = "select * from weissue.issue where appid=\'" + appid + "\';"; 
+        connection.query(sql, function(err, rows, fields){
+          if (err) {
+            msg = err;
+            connection.end();
+            status = 405;
+          } else {
+            msg = rows;
+            console.log('', rows); 
+          }
+          sendResponse(res, status, JSON.stringify(msg));
+        });
+      }
       break;
     }
-
-    // for asynchronous service, finish later
-    if (finished) {
-      sendResponse(res, status, msg);
-  }
   });
 }).listen(7000);
 
-console.log("We issue http server listening at 7000");
+ console.log("We issue http server listening at 7000");
 
-function sendResponse(res, status, msg) {
+ function sendResponse(res, status, msg) {
   res.writeHead(status, {
-      "Content-Type": "text/plain;charset=utf-8"
+    "Content-Type": "text/plain;charset=utf-8"
   });
   console.log((new Date()) + ' ' + msg);
   res.end(msg);
