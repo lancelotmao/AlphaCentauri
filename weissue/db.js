4f3dcf6b540a93db7a918856114ff9ba607ad3b0
Lance <lancelotmao@outlook.com>
2016-03-02 21:26:30 +0800

    weissue v0.1

---
+++ weissue/db.js
@@ -0,0 +1,113 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: '120.25.219.17',
+    port: 3306,
+    user: 'lara',
+    password: '!QAZ2wsx',
+    database: 'weissue'
+});
+
+try {
+    connection.connect();
+} catch(e) {}
+
+exports.getUsers = function(appid, callback) {
+    // stores user info for sending
+    var users = [];
+
+    var sql = "select * from weissue.app_user where appid=\'" + appid + "\';"; 
+    connection.query(sql, function(err, rows, fields) {
+        users = [];
+        userIndex = 0;
+        totalUser = rows.length;
+        for (var i = 0;i < rows.length;++i) {
+            var user = rows[i];
+            console.log('getting user info for: ' + user.userid);
+            var uSQL = "select uuid,name,email from common.user where uuid=\'" + user.userid + "\';";
+            connection.query(uSQL, function(err, row, fields) {
+                if (row.length > 0) {
+                    users.push(row[0]);
+                    userIndex++;
+                    if (userIndex >= totalUser) {
+                        console.log('Got all user, now sending response');
+                        callback(JSON.stringify(users));
+                    }
+                }
+            });
+        }
+    });
+}
+
+exports.create = function(title, description, submittedBy, handler, appid, versionCode, callback) {
+    if (title == null || description == null || submittedBy == null || handler == null || appid == null || versionCode == null) {
+        callback('title, description, submittedBy, handler, appid, versionCode cannot be null');
+    } else {
+        var uuid = guid();
+        var sql = "INSERT INTO weissue.issue (uid, title, description, submittedBy, handler, status, appid, versionCode, createdAt) VALUES ("
+        + "\'" + uuid + "\'," + "\'" + title + "\', " + "\'" + description + "\'," + "\'" + submittedBy + "\', " + "\'" + handler + "\', " + "\'open\'," + "\'" + appid + "\',"
+        + "\'" + versionCode + "\'," + "now()" + ");"; 
+        connection.query(sql, function(err, rows, fields){
+            if (err) {
+                console.log(err);
+            } else { 
+                callback("" + JSON.stringify({uuid:uuid}));
+            }
+        });
+    }
+}
+
+exports.list = function(appid, callback) {
+    var sql = "select * from weissue.issue where appid=\'" + appid + "\';"; 
+    connection.query(sql, function(err, rows, fields){
+        if (err) {
+            console.log(err);
+            callback('db error. create');
+        } else {
+            callback(JSON.stringify(rows));
+        }
+    });
+}
+
+exports.uploadScreenshot = function(issueid, callback) {
+    var uuid = guid();
+    var sql = "INSERT INTO weissue.issue_screenshot (issueid, ssid) VALUES ("
+    + "\'" + issueid + "\'," + "\'" + uuid + "\'" + ");"; 
+    connection.query(sql, function(err, rows, fields){
+        if (err) {
+            console.log(err);
+            callback('db error. uploadScreenshot');
+        } else {
+            callback(JSON.stringify({uuid:uuid}));
+        }
+    });
+}
+
+exports.screenshotList = function(issueid, callback) {
+    var sql = "select ssid from weissue.issue_screenshot where issueid=\'" + issueid + "\';";
+    connection.query(sql, function(err, rows, fields){
+        if (err) {
+            console.log(err);
+            callback('db error. screenshotList');
+        } else {
+            callback(rows);
+        }
+    });
+}
+
+function guid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+}
+
+function getUser(users, userid) {
+    var uSQL = "select * from common.user where uuid=\'" + userid + "\';";
+    connection.query(uSQL, function(err, row, fields) {
+        if (row.length > 0) {
+            users.push(row[0]);
+        }
+    });
+}
+
--- weissue/weissue.js
+++ weissue/weissue.js
@@ -1,30 +1,21 @@
 /*
  * http server
  */
- var http = require('http');
- var url = require("url");
- var mysql = require('mysql');
- var fs = require('fs');
+var http = require('http');
+var url = require("url");
+var mysql = require('mysql');
+var fs = require('fs');
+var db = require('./db.js');
 
- var connection = mysql.createConnection({
-  host: '120.25.219.17',
-  port: 3306,
-  user: 'lara',
-  password: '!QAZ2wsx',
-  database: 'weissue'
-});
+if (!fs.existsSync('screenshot')) {
+  fs.mkdir('screenshot', null);
+}
 
-try {
-  connection.connect();
-} catch(e) {}
-
- http.createServer(function (req, res) {
+http.createServer(function (req, res) {
   var parsedUrl = url.parse(req.url, true);
   var pathname = parsedUrl.pathname;
   var appid = parsedUrl.query.appid;
-
-  // stores user info for sending
-  var users = [];
+  var type = parsedUrl.query.type;
 
   // current index retrieved from DB
   var userIndex;
@@ -38,23 +29,28 @@ try {
 
   if (appid != null && !fs.existsSync(appid)) {
     fs.mkdir(appid, null);
-    fs.mkdir(appid + '/screenshot', null);
-    fs.mkdir(appid + '/voice', null);
   }
 
   var postData = "";
+  var issueID = parsedUrl.query.issueid;
   var uploadingUUID;
 
   switch (pathname) {
     case '/weissue/api/upload_screenshot':
-    uploadingUUID = guid();
+    {
+      uploadingUUID = guid();
+      var path = 'screenshot/' + issueID;
+      if (!fs.existsSync(path)) {
+        fs.mkdir(path);
+      }
+    }
     break;
   }
 
   req.on("data", function (postDataChunk) {
     switch (parsedUrl.pathname) {
       case "/weissue/api/upload_screenshot":
-      fs.appendFileSync(appid + '/screenshot/' + uploadingUUID, postDataChunk);
+      fs.appendFileSync('screenshot/' + issueID + '/' + uploadingUUID + '.' + type, postDataChunk);
       break;
 
       default:
@@ -80,28 +76,14 @@ try {
     
     switch (parsedUrl.pathname) {
       case '/weissue/api/users':
-      {
-        var sql = "select * from weissue.app_user where appid=\'" + appid + "\';"; 
-        connection.query(sql, function(err, rows, fields) {
-            users = [];
-            userIndex = 0;
-            totalUser = rows.length;
-            for (var i = 0;i < rows.length;++i) {
-              var user = rows[i];
-              console.log('getting user info for: ' + user.userid);
-              var uSQL = "select uuid,name,email from common.user where uuid=\'" + user.userid + "\';";
-              connection.query(uSQL, function(err, row, fields) {
-                if (row.length > 0) {
-                  users.push(row[0]);
-                  userIndex++;
-                  if (userIndex == totalUser) {
-                    console.log('Got all user, now sending response');
-                    sendResponse(res, status, JSON.stringify(users));
-                  }
-                }
-              });
-            }
-        });
+      { 
+        if (appid) {
+          db.getUsers(appid, function(data) {
+            sendResponse(res, status, data);
+          });
+        } else {
+            sendResponse(res, status, 'must specify appid');
+        }
       }
       break;
 
@@ -112,82 +94,74 @@ try {
         var submittedBy = postObj['submittedBy'];
         var handler = postObj['handler'];
         var versionCode = postObj['versionCode'];
-        if (title == null || description == null || submittedBy == null || handler == null || appid == null || versionCode == null) {
-          msg = 'title, description, submittedBy, handler, appid, versionCode cannot be null';
-          sendResponse(res, 405, msg);
-        } else {
-          var uuid = guid();
-          var sql = "INSERT INTO weissue.issue (uid, title, description, submittedBy, handler, status, appid, versionCode, createdAt) VALUES ("
-          + "\'" + uuid + "\'," + "\'" + title + "\', " + "\'" + description + "\'," + "\'" + submittedBy + "\', " + "\'" + handler + "\', " + "\'open\'," + "\'" + appid + "\',"
-          + "\'" + versionCode + "\'," + "now()" + ");"; 
-          connection.query(sql, function(err, rows, fields){
-            if (err) {
-              console.log(err);
-              connection.end();
-            } else { 
-              msg = 'create issue success';
-              sendResponse(res, status, JSON.stringify({uuid:uuid}));
-            }
-          });
-        }
+        db.create(title, description, submittedBy, handler, appid, versionCode, function(data) {
+          sendResponse(res, status, data);
+        });
       }
       break;
 
       case '/weissue/api/list':
       {
-        var sql = "select * from weissue.issue where appid=\'" + appid + "\';"; 
-        connection.query(sql, function(err, rows, fields){
-          if (err) {
-            msg = err;
-            connection.end();
-            status = 405;
-          } else {
-            msg = rows;
-            console.log('', rows); 
-          }
-          sendResponse(res, status, JSON.stringify(msg));
+        db.list(appid, function(data) {
+          sendResponse(res, status, data);
         });
       }
       break;
 
       case "/weissue/api/upload_screenshot":
       {
-        msg = 'upload screen shot success';
-        sendResponse(res, status, msg);
+        db.uploadScreenshot(issueID, function(data){
+          msg = data;
+          sendResponse(res, status, msg);
+        });
       }
       break;
 
       case "/weissue/api/download_screenshot":
       {
-        var ssdir = appid + '/screenshot';
-        var ss = fs.readdirSync(ssdir);
-        if (ss.length == 0) {
-          status = 404;
-          msg = 'screen shot download failed: ' + appid;
-          sendResponse(res, status, msg);
+        var ssdir = 'screenshot/' + issueID;
+        if (fs.existsSync(ssdir)) {
+          var ss = fs.readdirSync(ssdir);
+          if (ss.length == 0) {
+            status = 404;
+            msg = 'screen shot download failed: ' + issueID;
+            sendResponse(res, status, msg);
+          } else {
+            finished = false;
+            var path = ssdir + '/' + ss[0];
+            console.log('downloading screen shot: ' + path);
+            fs.readFile(path, "binary", function (err, file) {
+              if (err) {
+                status = 404;
+                msg = 'download failed: ' + path;
+                res.writeHead(status, {
+                  "Content-Type": "text/plain;charset=utf-8"
+                });
+                res.end(msg);
+              } else {
+                res.writeHead(status, {
+                  "Content-Type": "application/octet-stream",
+                  "Content-Length": fs.statSync(path)['size']
+                });
+                res.write(file, "binary");
+                msg = 'download success';
+                res.end();
+              }
+            });
+          }
         } else {
-          finished = false;
-          var path = ssdir + '/' + ss[0];
-          console.log('downloading screen shot: ' + path);
-          fs.readFile(path, "binary", function (err, file) {
-            if (err) {
-              status = 404;
-              msg = 'download failed: ' + path;
-              res.writeHead(status, {
-                "Content-Type": "text/plain;charset=utf-8"
-              });
-              res.end(msg);
-            } else {
-              res.writeHead(status, {
-                "Content-Type": "application/octet-stream",
-                "Content-Length": fs.statSync(path)['size']
-              });
-              res.write(file, "binary");
-              msg = 'download success';
-              res.end();
-            }
-          });
+          status = 404;
+          msg = 'screen shot download failed: ' + issueID;
+          sendResponse(res, status, msg);
         }
+      }
+      break;
+
+      case '/weissue/api/screenshotList':
+      {
+        db.screenshotList(issueID, function(data) {
+          sendResponse(res, status, JSON.stringify(data));
+        });
       }
       break;
 
